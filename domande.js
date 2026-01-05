9e2dc1c23b8b89617f3f9104dd6ff14303aa1d7f
Carmine D'alise <156032647+iacreatorcar@users.noreply.github.com>
2026-01-05 10:01:22 +0100

    Update domande.js

--- domande.js
+++ domande.js
@@ -1,1 +1,17 @@
-
+const questions = [
+    { q: "Dove è stato vinto lo scudetto il 4 maggio 2023?", options: ["Napoli", "Udine", "Salerno", "Roma"], correct: 1 },
+    { q: "Chi era l'allenatore dello scudetto 2023?", options: ["Garcia", "Spalletti", "Mazzarri", "Conte"], correct: 1 },
+    { q: "Chi è stato il capocannoniere 2022/23?", options: ["Kvara", "Simeone", "Osimhen", "Lozano"], correct: 2 },
+    { q: "Chi è l'attuale allenatore del Napoli?", options: ["Calzona", "Conte", "Manna", "De Rossi"], correct: 1 },
+    { q: "Squadra di provenienza di McTominay?", options: ["Chelsea", "Man. United", "Arsenal", "Celtic"], correct: 1 },
+    { q: "Chi è il nuovo difensore centrale dal Torino?", options: ["Rafa Marin", "Buongiorno", "Natan", "Hermoso"], correct: 1 },
+    { q: "Punti fatti nel campionato dello scudetto?", options: ["86", "90", "91", "88"], correct: 2 },
+    { q: "Contro chi ha debuttato Conte in Serie A col Napoli?", options: ["Modena", "Verona", "Bologna", "Parma"], correct: 1 },
+    { q: "In che squadra gioca ora Victor Osimhen?", options: ["PSG", "Galatasaray", "Chelsea", "Al-Ahli"], correct: 1 },
+    { q: "Chi è il capitano del Napoli?", options: ["Di Lorenzo", "Lobotka", "Rrahmani", "Politano"], correct: 0 },
+    { q: "Da dove è stato acquistato Lukaku?", options: ["Inter", "Chelsea", "Roma", "Everton"], correct: 1 },
+    { q: "Chi ha segnato il gol del pareggio a Udine?", options: ["Osimhen", "Kvara", "Zielinski", "Di Lorenzo"], correct: 0 },
+    { q: "Il nuovo esterno arrivato dal Benfica?", options: ["Neres", "Spinazzola", "Mazzocchi", "Politano"], correct: 0 },
+    { q: "Il nome del nuovo DS azzurro?", options: ["Giuntoli", "Manna", "Meluso", "Sartori"], correct: 1 },
+    { q: "Il nome dello stadio oggi?", options: ["San Paolo", "Diego Armando Maradona", "Vesuvio", "Fuorigrotta"], correct: 1 }
+];
